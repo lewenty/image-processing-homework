51ff5bbce799e9f7115e6bd3d1f318d31adc3997
levent.yildiz <levent.yildiz@turkkep.com.tr>
2014-10-29 21:19:10 +0200

    structure updated..

--- src/com/homeworks/hw1/HomeWork1.java
+++ src/com/homeworks/hw1/HomeWork1.java
@@ -1,19 +1,5 @@
 package com.homeworks.hw1;
 
-import vpt.ByteImage;
-import vpt.Image;
-import vpt.algorithms.display.Display2D;
-import vpt.algorithms.io.Load;
-
-
 public class HomeWork1 {
 
-	public static void main(String[] args) {
-		System.out.println("running");
-		
-		Image img = Load.invoke("ornekler/lenna.png");
-		Display2D.invoke(img);
-		
-		
-	}
 }
---
+++ src/com/homeworks/run/RunHw1.java
@@ -0,0 +1,12 @@
+package com.homeworks.run;
+
+import vpt.Image;
+import vpt.algorithms.display.Display2D;
+import vpt.algorithms.io.Load;
+
+public class RunHw1 {
+
+    public static void main(String[] args) {
+        System.out.println("running..");
+    }
+}
